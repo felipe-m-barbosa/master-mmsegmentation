da73c88fdc6bd348c7686dec3dc1bf1842f92557
unknown <felipemanbar@gmail.com>
2023-01-04 00:39:55 -0300

    Correct parameters sent to _decode_head_forward_train()

--- mmsegmentation-clone/mmseg/models/segmentors/encoder_decoder.py
+++ mmsegmentation-clone/mmseg/models/segmentors/encoder_decoder.py
@@ -165,7 +165,10 @@ class EncoderDecoder(BaseSegmentor):
         losses = dict()
 
         loss_decode = self._decode_head_forward_train(x, img_metas,
-                                                      gt_semantic_seg)
+                                                      gt_semantic_seg,
+                                                      s1=s1, 
+                                                      s2=s2, 
+                                                      opt_flow=kwargs['opt_flow'])
         losses.update(loss_decode)
 
         if self.with_auxiliary_head:
