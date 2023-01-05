62a45080810818710f9a0d73b34b6583e0c3e42c
unknown <felipemanbar@gmail.com>
2023-01-04 22:30:15 -0300

    debugging

--- mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
+++ mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
@@ -337,6 +337,7 @@ class BaseDecodeHead(BaseModule, metaclass=ABCMeta):
 
         jaccard = JaccardIndex(task='multiclass', num_classes=self.num_classes)
         seg_logit = F.softmax(seg_logit, dim=1)
+        print(torch.unique(seg_label))
         loss['miou'] = jaccard(seg_logit, seg_label)
 
         return loss
