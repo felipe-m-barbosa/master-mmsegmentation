05bbf0ab224a2ed8207e0ec2e4dfb1dbefd16898
unknown <felipemanbar@gmail.com>
2023-01-20 09:39:13 -0300

    Add custom datasets

--- mmsegmentation-clone/mmseg/datasets/cityscapes.py
+++ mmsegmentation-clone/mmseg/datasets/cityscapes.py
@@ -4,10 +4,14 @@ import os.path as osp
 import mmcv
 import numpy as np
 from mmcv.utils import print_log
+from mmseg.utils import get_root_logger
 from PIL import Image
+import random
 
 from .builder import DATASETS
-from .custom import CustomDataset
+from .custom import CustomDataset, newCustomDataset
+from .pipelines import newLoadAnnotations
+
 
 
 @DATASETS.register_module()
@@ -212,3 +216,300 @@ class CityscapesDataset(CustomDataset):
             CSEval.evaluateImgLists(pred_list, seg_map_list, CSEval.args))
 
         return eval_results
+
+
+@DATASETS.register_module()
+class newCityscapesDataset(newCustomDataset):
+    """Cityscapes dataset.
+    The ``img_suffix`` is fixed to '_leftImg8bit.png' and ``seg_map_suffix`` is
+    fixed to '_gtFine_labelTrainIds.png' for Cityscapes dataset.
+    """
+
+    CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+               'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
+               'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
+               'bicycle')
+
+    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+               [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
+               [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
+               [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
+               [0, 80, 100], [0, 0, 230], [119, 11, 32]]
+
+    def __init__(self,
+                 img_suffix='_leftImg8bit.png',
+                 seg_map_suffix='_gtFine_labelTrainIds.png',
+                 **kwargs):
+        super(newCityscapesDataset, self).__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+
+    @staticmethod
+    def _convert_to_label_id(result):
+        """Convert trainId to id for cityscapes."""
+        if isinstance(result, str):
+            result = np.load(result)
+        import cityscapesscripts.helpers.labels as CSLabels
+        result_copy = result.copy()
+        for trainId, label in CSLabels.trainId2label.items():
+            result_copy[result == trainId] = label.id
+
+        return result_copy
+
+    def results2img(self, results, imgfile_prefix, to_label_id, indices=None):
+        """Write the segmentation results to images.
+        Args:
+            results (list[ndarray]): Testing results of the
+                dataset.
+            imgfile_prefix (str): The filename prefix of the png files.
+                If the prefix is "somepath/xxx",
+                the png files will be named "somepath/xxx.png".
+            to_label_id (bool): whether convert output to label_id for
+                submission.
+            indices (list[int], optional): Indices of input results,
+                if not set, all the indices of the dataset will be used.
+                Default: None.
+        Returns:
+            list[str: str]: result txt files which contains corresponding
+            semantic segmentation images.
+        """
+        if indices is None:
+            indices = list(range(len(self)))
+
+        mmcv.mkdir_or_exist(imgfile_prefix)
+        result_files = []
+        for result, idx in zip(results, indices):
+            if to_label_id:
+                result = self._convert_to_label_id(result)
+            filename = self.img_infos[idx]['filename']
+            basename = osp.splitext(osp.basename(filename))[0]
+
+            png_filename = osp.join(imgfile_prefix, f'{basename}.png')
+
+            output = Image.fromarray(result.astype(np.uint8)).convert('P')
+            import cityscapesscripts.helpers.labels as CSLabels
+            palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
+            for label_id, label in CSLabels.id2label.items():
+                palette[label_id] = label.color
+
+            output.putpalette(palette)
+            output.save(png_filename)
+            result_files.append(png_filename)
+
+        return result_files
+
+    def format_results(self,
+                       results,
+                       imgfile_prefix,
+                       to_label_id=True,
+                       indices=None):
+        """Format the results into dir (standard format for Cityscapes
+        evaluation).
+        Args:
+            results (list): Testing results of the dataset.
+            imgfile_prefix (str): The prefix of images files. It
+                includes the file path and the prefix of filename, e.g.,
+                "a/b/prefix".
+            to_label_id (bool): whether convert output to label_id for
+                submission. Default: False
+            indices (list[int], optional): Indices of input results,
+                if not set, all the indices of the dataset will be used.
+                Default: None.
+        Returns:
+            tuple: (result_files, tmp_dir), result_files is a list containing
+                the image paths, tmp_dir is the temporal directory created
+                for saving json/png files when img_prefix is not specified.
+        """
+        if indices is None:
+            indices = list(range(len(self)))
+
+        assert isinstance(results, list), 'results must be a list.'
+        assert isinstance(indices, list), 'indices must be a list.'
+
+        result_files = self.results2img(results, imgfile_prefix, to_label_id,
+                                        indices)
+
+        return result_files
+
+    def evaluate(self,
+                 results,
+                 metric='mIoU',
+                 logger=None,
+                 imgfile_prefix=None):
+        """Evaluation in Cityscapes/default protocol.
+        Args:
+            results (list): Testing results of the dataset.
+            metric (str | list[str]): Metrics to be evaluated.
+            logger (logging.Logger | None | str): Logger used for printing
+                related information during evaluation. Default: None.
+            imgfile_prefix (str | None): The prefix of output image file,
+                for cityscapes evaluation only. It includes the file path and
+                the prefix of filename, e.g., "a/b/prefix".
+                If results are evaluated with cityscapes protocol, it would be
+                the prefix of output png files. The output files would be
+                png images under folder "a/b/prefix/xxx.png", where "xxx" is
+                the image name of cityscapes. If not specified, a temp file
+                will be created for evaluation.
+                Default: None.
+        Returns:
+            dict[str, float]: Cityscapes/default metrics.
+        """
+
+        eval_results = dict()
+        metrics = metric.copy() if isinstance(metric, list) else [metric]
+        if 'cityscapes' in metrics:
+            eval_results.update(
+                self._evaluate_cityscapes(results, logger, imgfile_prefix))
+            metrics.remove('cityscapes')
+        if len(metrics) > 0:
+            eval_results.update(
+                super(newCityscapesDataset,
+                      self).evaluate(results, metrics, logger))
+
+        return eval_results
+
+    def _evaluate_cityscapes(self, results, logger, imgfile_prefix):
+        """Evaluation in Cityscapes protocol.
+        Args:
+            results (list): Testing results of the dataset.
+            logger (logging.Logger | str | None): Logger used for printing
+                related information during evaluation. Default: None.
+            imgfile_prefix (str | None): The prefix of output image file
+        Returns:
+            dict[str: float]: Cityscapes evaluation results.
+        """
+        try:
+            import cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling as CSEval  # noqa
+        except ImportError:
+            raise ImportError('Please run "pip install cityscapesscripts" to '
+                              'install cityscapesscripts first.')
+        msg = 'Evaluating in Cityscapes style'
+        if logger is None:
+            msg = '\n' + msg
+        print_log(msg, logger=logger)
+
+        result_dir = imgfile_prefix
+
+        eval_results = dict()
+        print_log(f'Evaluating results under {result_dir} ...', logger=logger)
+
+        CSEval.args.evalInstLevelScore = True
+        CSEval.args.predictionPath = osp.abspath(result_dir)
+        CSEval.args.evalPixelAccuracy = True
+        CSEval.args.JSONOutput = False
+
+        seg_map_list = []
+        pred_list = []
+
+        # when evaluating with official cityscapesscripts,
+        # **_gtFine_labelIds.png is used
+        for seg_map in mmcv.scandir(
+                self.ann_dir, 'gtFine_labelIds.png', recursive=True):
+            seg_map_list.append(osp.join(self.ann_dir, seg_map))
+            pred_list.append(CSEval.getPrediction(CSEval.args, seg_map))
+
+        eval_results.update(
+            CSEval.evaluateImgLists(pred_list, seg_map_list, CSEval.args))
+
+        return eval_results
+
+from mmseg.core.evaluation import get_palette
+
+
+@DATASETS.register_module()
+class newCityscapesDataset1(newCityscapesDataset):
+  CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+               'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
+               'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
+               'bicycle')
+  PALETTE = get_palette('cityscapes')
+
+  def __init__(self, img_suffix='.png', seg_map_suffix='.png', **kwargs):
+    super().__init__(img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+    assert osp.exists(self.img_dir) and self.split is not None
+
+    self.gt_seg_map_loader = newLoadAnnotations()
+    self.seqs_list = [] # stores the filenames of images inside the sequences folder
+    self.seq_dir = kwargs['seq_dir'] # comes from kwargs (I think)
+    self.optflow_dir = kwargs['optflow_dir'] # comes from kwargs (I think)
+
+
+    # load annotations
+    self.img_infos = self.load_annotations(self.img_dir, self.img_suffix,
+                                            self.ann_dir,
+                                            self.seg_map_suffix, self.seq_dir, self.optflow_dir, self.split)
+  
+
+
+  # here, we only set the filenames
+  def load_annotations(self, img_dir, img_suffix, ann_dir, seg_map_suffix, seq_dir, optflow_dir, split):
+        """Load annotation from directory.
+        Args:
+            img_dir (str): Path to image directory
+            img_suffix (str): Suffix of images.
+            ann_dir (str|None): Path to annotation directory.
+            seg_map_suffix (str|None): Suffix of segmentation maps.
+            split (str|None): Split txt file. If split is specified, only file
+                with suffix in the splits will be loaded. Otherwise, all images
+                in img_dir/ann_dir will be loaded. Default: None
+        Returns:
+            list[dict]: All image info of dataset.
+        """
+
+        img_infos = []
+        
+        if split is not None:
+            lines = mmcv.list_from_file(
+                split, file_client_args=self.file_client_args)
+            for line in lines:
+                img_name = line.strip()
+                img_info = dict(filename=img_name + img_suffix)
+                
+                if ann_dir is not None:
+                    seg_map = img_name.replace('leftImg8bit', 'gtFine_labelIds') + seg_map_suffix
+                    img_info['ann'] = dict(seg_map=seg_map)
+                
+
+                if seq_dir is not None:
+                  self.seqs_list = sorted(os.listdir(seq_dir))
+                  idx = random.randint(0,len(self.seqs_list)-2)
+                  # sequences information
+                  img_info['s1'] = dict(filename=osp.join(seq_dir, self.seqs_list[idx]))
+                  img_info['s2'] = dict(filename=osp.join(seq_dir, self.seqs_list[idx+1]))
+                
+                  if optflow_dir is not None:
+                    self.optflow_list = sorted(os.listdir(optflow_dir))
+                    img_info['optflow'] = dict(filename=osp.join(optflow_dir, self.optflow_list[idx])) # the optical flow is computed from frame in t to t+1,
+                    # hence, we select the optical flow corresponding to frame t (in this case, idx)
+
+                img_infos.append(img_info)
+
+        else: #TO DO (DOING ... DONE)
+            for img in self.file_client.list_dir_or_file(
+                    dir_path=img_dir,
+                    list_dir=False,
+                    suffix=img_suffix,
+                    recursive=True):
+                img_info = dict(filename=img)
+
+                if ann_dir is not None:
+                    seg_map = img.replace('leftImg8bit', 'gtFine_labelIds')
+                    img_info['ann'] = dict(seg_map=seg_map)
+                
+                if seq_dir is not None:
+                  self.seqs_list = sorted(os.listdir(seq_dir))
+                  idx = random.randint(0,len(self.seqs_list)-2)
+                  # sequences information
+                  img_info['s1'] = dict(filename=osp.join(seq_dir, self.seqs_list[idx]))
+                  img_info['s2'] = dict(filename=osp.join(seq_dir, self.seqs_list[idx+1]))
+                
+                  if optflow_dir is not None:
+                    self.optflow_list = sorted(os.listdir(optflow_dir))
+                    img_info['optflow'] = dict(filename=osp.join(optflow_dir, self.optflow_list[idx])) # the optical flow is computed from frame in t to t+1,
+                    # hence, we select the optical flow corresponding to frame t (in this case, idx)
+
+                img_infos.append(img_info)
+
+            img_infos = sorted(img_infos, key=lambda x: x['filename'])
+
+        print_log(f'Loaded {len(img_infos)} images', logger=get_root_logger())
+        return img_infos
