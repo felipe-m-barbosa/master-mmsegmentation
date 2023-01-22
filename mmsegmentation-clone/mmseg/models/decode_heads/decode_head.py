b10a124dbcd2af4fbb3915bcdc767137dfa0f729
unknown <felipemanbar@gmail.com>
2023-01-21 22:32:55 -0300

    Debug

--- mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
+++ mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
@@ -276,13 +276,14 @@ class BaseDecodeHead(BaseModule, metaclass=ABCMeta):
         """Compute segmentation loss."""
         loss = dict()
         print('Seg logit shape: ', seg_logit.shape)
+        print('Seg label shape: ', seg_label.shape)
         seg_logit = resize(
             input=seg_logit,
             size=seg_label.shape[2:],
             mode='bilinear',
             align_corners=self.align_corners)
         
-        print('Seg logit shape: ', seg_logit.shape)
+        # print('Seg logit shape: ', seg_logit.shape)
 
         s1_logits = resize(
             input=kwargs['s1_logits'],
