8b259e77b5db114cb3d6bea06b1dbd223ef83b02
unknown <felipemanbar@gmail.com>
2023-02-12 23:09:49 -0300

    Add NLLLoss to losses init

--- mmsegmentation-clone/mmseg/models/losses/__init__.py
+++ mmsegmentation-clone/mmseg/models/losses/__init__.py
@@ -8,11 +8,12 @@ from .lovasz_loss import LovaszLoss
 from .tversky_loss import TverskyLoss
 from .temporal_consistency_loss import TCLoss
 from .l1loss import L1Loss
+from .nll_loss import NLLLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
     'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss',
-    'FocalLoss', 'TverskyLoss', 'TCLoss', 'L1Loss'
+    'FocalLoss', 'TverskyLoss', 'TCLoss', 'L1Loss', 'NLLLoss'
 ]
