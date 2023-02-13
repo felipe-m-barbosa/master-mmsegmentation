1cb5dd38a24fc232bcb65c2d6b0d6ab014282945
unknown <felipemanbar@gmail.com>
2023-02-12 23:07:31 -0300

    Add NLLLoss

---
+++ mmsegmentation-clone/mmseg/models/losses/nll_loss.py
@@ -0,0 +1,82 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ..builder import LOSSES
+from .utils import get_class_weight, weight_reduce_loss
+
+
+@LOSSES.register_module()
+class NLLLoss(nn.Module):
+
+    def __init__(self,
+                 use_sigmoid=False,
+                 use_mask=False,
+                 reduction='mean',
+                 class_weight=None,
+                 loss_weight=1.0,
+                 loss_name='loss_nll',
+                 avg_non_ignore=False):
+        super(NLLLoss, self).__init__()
+        assert (use_sigmoid is False) or (use_mask is False)
+        self.use_sigmoid = use_sigmoid
+        self.use_mask = use_mask
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+        self.class_weight = get_class_weight(class_weight)
+        self.avg_non_ignore = avg_non_ignore
+        if not self.avg_non_ignore and self.reduction == 'mean':
+            warnings.warn(
+                'Default ``avg_non_ignore`` is False, if you would like to '
+                'ignore the certain label and average loss over non-ignore '
+                'labels, which is the same with PyTorch official '
+                'cross_entropy, set ``avg_non_ignore=True``.')
+
+        self.cls_criterion = nn.NLLLoss()
+        self._loss_name = loss_name
+
+    def extra_repr(self):
+        """Extra repr."""
+        s = f'avg_non_ignore={self.avg_non_ignore}'
+        return s
+
+    def forward(self,
+                cls_score,
+                label,
+                **kwargs):
+        """Forward function."""
+
+
+        # print(kwargs.keys())
+
+        label = torch.argmax(label, dim=1)
+        m = nn.LogSoftmax(dim=1)
+
+        # print(f"label dim {label.shape}")
+        # print(f"cls_score dim {cls_score.shape}")
+
+
+        loss_cls = self.loss_weight * self.cls_criterion(m(cls_score), label)
+        
+        loss_cls = weight_reduce_loss(
+        loss_cls, weight=None, reduction=self.reduction, avg_factor=None)
+
+        return loss_cls
+
+    @property
+    def loss_name(self):
+        """Loss Name.
+
+        This function must be implemented and will return the name of this
+        loss function. This name will be used to combine different loss items
+        by simple sum operation. In addition, if you want this loss item to be
+        included into the backward graph, `loss_` must be the prefix of the
+        name.
+
+        Returns:
+            str: The name of this loss item.
+        """
+        return self._loss_name
