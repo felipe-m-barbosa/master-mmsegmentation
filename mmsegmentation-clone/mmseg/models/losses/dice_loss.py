9071511a5f4d288198499b665818703805690695
unknown <felipemanbar@gmail.com>
2023-02-27 14:51:54 -0300

    Remove prints used for debug

--- mmsegmentation-clone/mmseg/models/losses/dice_loss.py
+++ mmsegmentation-clone/mmseg/models/losses/dice_loss.py
@@ -70,14 +70,9 @@ def warp(x, flo, inp1, inp2):
     mask[mask<0.9999]=0
     mask[mask>0]=1
 
-    print("MASK SHAPE: ", mask.shape)
-
     # OCCLUSION MASK, SUCH AS IN [An Unsupervised Temporal Consistency (TC) Loss to Improve the Performance of Semantic Segmentation Networks]
     mask = torch.exp(-torch.norm(inp1 - inp2, p=1, dim=1))
     mask = mask.unsqueeze(1).repeat(1,output.shape[1],1,1)
-
-    print("MASK SHAPE: ", mask.shape)
-    print("OUTPUT SHAPE: ", output.shape)
 
     # VISIBILITY MASK, SUCH AS IN [Learning Blind Video Temporal Consistency]
 
