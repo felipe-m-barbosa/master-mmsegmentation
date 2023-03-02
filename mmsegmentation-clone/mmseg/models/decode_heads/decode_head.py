b7012703bef2b0f0cf53f4637452e16af85d2443
unknown <felipemanbar@gmail.com>
2023-03-02 16:52:38 -0300

    Undo remove *

--- mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
+++ mmsegmentation-clone/mmseg/models/decode_heads/decode_head.py
@@ -60,6 +60,7 @@ class BaseDecodeHead(BaseModule, metaclass=ABCMeta):
     def __init__(self,
                  in_channels,
                  channels,
+                 *,
                  num_classes,
                  out_channels=None,
                  threshold=None,
