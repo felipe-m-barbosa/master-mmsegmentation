24a86dd635ad5c6975ef867e0571d86d293d5019
unknown <felipemanbar@gmail.com>
2023-03-04 12:02:58 -0300

    Debug

--- mmsegmentation-clone/mmseg/datasets/pipelines/loading.py
+++ mmsegmentation-clone/mmseg/datasets/pipelines/loading.py
@@ -464,6 +464,9 @@ class newLoadAnnotations(object):
 
             results['gt_depth'] = gt_depth
 
+            print('\n\n')
+            print(results['gt_depth'], end='\n\n\n')
+
         if results.get('seg_prefix', None) is not None:
             filename = osp.join(results['seg_prefix'],
                                 results['ann_info']['seg_map'])
