499f3873af42072f4849de34d59e625cbcf03409
unknown <felipemanbar@gmail.com>
2023-06-23 13:51:13 -0300

    Clear Debug

--- mmsegmentation-clone/mmseg/datasets/pipelines/transforms.py
+++ mmsegmentation-clone/mmseg/datasets/pipelines/transforms.py
@@ -2050,9 +2050,9 @@ class newNormalize(object):
         
         if 's1_img' in results:
             # how to condition it only for data from the target data (using the results dict -> modify data loading)
-            print("\n\n\n")
-            print(f"self.mean: {self.mean}")
-            print("\n\n\n")
+            # print("\n\n\n")
+            # print(f"self.mean: {self.mean}")
+            # print("\n\n\n")
             if results['from_target']:
                 mean = np.array([102.4890, 102.9852,  96.2737])
                 std = np.array([88.5729, 86.4109, 81.2531])
